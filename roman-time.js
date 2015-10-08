e16a49112a64599b5ad2147db987b1f8c2d359bf
m-a-bykovsky <m.a.bykovsky@gmail.com>
2015-10-09 02:20:44 +0500

    fixed codestyle

--- roman-time.js
+++ roman-time.js
@@ -5,62 +5,62 @@ var minutes = process.argv[3];
 
 // Немного замечательного кода и магии by m-a-bykovsky
 if (( 0 <= hours && hours <= 23) && (0 <= minutes && minutes <= 59)) {
-	console.log(convertArabicToRoman(hours) +':'+ convertArabicToRoman(minutes));
+    console.log(convertArabicToRoman(hours) +':'+ convertArabicToRoman(minutes));
 } else {
-	console.log('Время указано не верно');
+    console.log('Время указано не верно');
 }
 
 function getRomanDigit(x) {
-	var romanDigit
-	switch (x) {
-		case 1:
-			romanDigit = "I";
-			break;
-		case 5:
-			romanDigit = "V";
-			break;
-		case 10:
-			romanDigit = "X";
-			break;
-		case 50:
-			romanDigit = "L";
-			break;
-	}
-	return (romanDigit)
+    var romanDigit
+    switch (x) {
+        case 1:
+            romanDigit = "I";
+            break;
+        case 5:
+            romanDigit = "V";
+            break;
+        case 10:
+            romanDigit = "X";
+            break;
+        case 50:
+            romanDigit = "L";
+            break;
+    }
+    return (romanDigit)
  }
  
 function convertArabicToRoman(x){
-	var rank;
-	var num = '';
-	x = parseInt(x);
-	//console.log(x);
-	if (x == 0){
-		num = 'NN';
-	}
-	else{
-		while (x > 0){
-			//Проверяем разрядность
-			rank = ( 1 <= x && x <= 9) ? 1:
+    var rank;
+    var num = '';
+    x = parseInt(x);
+    //console.log(x);
+    if (x == 0){
+        num = 'NN';
+    }
+    else{
+        while (x > 0){
+            //Проверяем разрядность
+            rank = ( 1 <= x && x <= 9) ? 1:
                 ( 10 <= x && x <= 59) ? 10:
                     console.log('Error ' + x.length + ' ' + typeof(x) + x) && process.abort(); //Отладочная
-			//Перевод в римские
-			if (x >= 9*rank){
-				num = num + getRomanDigit(rank) + getRomanDigit(rank*10);
-				x = (x - 9*rank);
-			}
-			else if (x >= 5*rank){
-				num = num + getRomanDigit(5*rank);
-				x = (x - 5*rank);
-			}
-			else if (x >= 4*rank){
-				num = num + getRomanDigit(rank) + getRomanDigit(5*rank); 
-				x = (x - 4*rank);
-			} 
-			else if (x >= rank){
-				num = num + getRomanDigit(rank);
-				x = (x - rank);	
-			}
-		}
-	}
-	return num
+            //Перевод в римские
+            if (x >= 9*rank){
+                num = num + getRomanDigit(rank) + getRomanDigit(rank*10);
+                x = (x - 9*rank);
+            }
+            else if (x >= 5*rank){
+                num = num + getRomanDigit(5*rank);
+                x = (x - 5*rank);
+            }
+            else if (x >= 4*rank){
+                num = num + getRomanDigit(rank) + getRomanDigit(5*rank); 
+                x = (x - 4*rank);
+            } 
+            else if (x >= rank){
+                num = num + getRomanDigit(rank);
+                x = (x - rank);
+            }
+        }
+    }
+    return num
 }
